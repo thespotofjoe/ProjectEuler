e4e892c90d53f72219b6336e3c1281f62feb636a
Joseph Buchoff <joseph.buchoff@rutgers.edu>
2016-01-11 22:21:11 -0500

    isPalindrome is working

--- Prob4.py
+++ Prob4.py
@@ -1,10 +1,13 @@
 def isPalindrome (n):
     i = 2
-    while 10**i < n:    #Find the number of digits in the number n
-        1
+    while 10**i < n:    #Find the number of digits in the number n, store in i
+        i += 1
+    i -= 1    #Edit i to reflect actual digits
     j = 0
-    while i != j:
-        if n%10**i != n%10**j:  #Figure out whether the digit at i is the same as
+    while i >= j:
+        if int(n%10**(i+1)/(10**i)) != int(n%10**(j+1)/(10**j)):  #Figure out whether the digit at i is the same as
+            #debug print ("i:", i, "\ni digit", int(n%10**(i+1)/(10**i)))
+            #debug print ("j:", j, "\nj digit", int(n%10**(j+1)/(10**j)))
             return False        #the digit at j, if not, return false
         i -= 1
         j += 1
